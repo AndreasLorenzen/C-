dca22e3d961b0b89ba2c62e03ee7badb8040bbb1
AndreasLorenzen <andreas.141106@gmail.com>
2024-08-15 13:48:39 +0200

    Add files via upload

---
+++ Variabel-Opgave-3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Opgave1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Navn = "Søren";
+            int Alder = 16;
+            double Penge = 1234.34;
+
+            Console.WriteLine($"Jeg hedder {Navn}, er {Alder} år gammel og har tjent {Penge} kr. på at lappe cykler.");
+            Console.ReadKey();
+        }
+    }
+}
